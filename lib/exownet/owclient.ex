433d19cebe66fb624ccd814568457c0614f0d1b5
Evan Horne <evan.horne@oooska.com>
2023-05-12 02:07:28 -0400

    Added some documentation and improved(?) typespecs.

--- lib/exownet/owclient.ex
+++ lib/exownet/owclient.ex
@@ -1,7 +1,32 @@
 
+
 defmodule Exownet.OWClient do
   defstruct [:address, :port, :flag, :socket, :persistence, :errors_map, :verbose]
   alias Exownet.OWPacket
+
+
+@moduledoc """
+A client for interacting with owserver. You probably want to use the `Exownet` application rather than use the client directly.
+
+`Exownet.OWClient` provides a struct representing the connection information for an owserver.
+
+## Types
+
+  - `t()`: The client struct, which holds the state of the connection.
+  - `error_tuple()`: Represents errors that can occur during the operation of the client.
+
+## Example
+
+Here's an example of how to use the client:
+
+```elixir
+client = Exownet.OWClient.new('localhost', 4304, [:persistence])
+{:ok, client, dir_content} = Exownet.OWClient.dir(client, "/")
+IO.inspect(dir_content)
+```
+This will connect to an OWServer running on localhost, port 4304, with connection persistence enabled, and then
+fetch a directory listing of the root directory.
+"""
 
   @type t :: %__MODULE__{
           address: charlist,
@@ -10,11 +35,26 @@ defmodule Exownet.OWClient do
           socket: :gen_tcp.socket() | nil,
         }
 
-  @type error_tuple :: {:error, any()}
-
+  @type owserver_error :: integer()
+  @type socket_error :: atom()
+  @type error_tuple :: {:error, String.t() | owserver_error() | socket_error()}
   @maxsize 65536
 
-  @spec new(charlist | String.t(), integer, list(atom)) :: t()
+  @doc """
+  Creates a new `OWClient` with the specified address, port, and flags.
+
+  ## Parameters
+
+  - `address`: A charlist or string representing the IP address or hostname of the OWServer. Defaults to `'localhost'`.
+  - `port`: An integer representing the port number of the OWServer. Defaults to `4304`.
+  - `flags`: A list of atoms representing flags for the client. Defaults to `[:persistence]`.
+
+  ## Returns
+
+  Returns a new `OWClient` struct with the specified address, port, and flags. The socket field of the struct is initially set to `nil`.
+
+  """
+  @spec new(charlist | String.t(), integer, OWPacket.flag_list()) :: t()
   def new(address \\ 'localhost', port \\ 4304, flags \\ [:persistence]) do
     %__MODULE__{
       address: to_charlist(address),
@@ -24,7 +64,22 @@ defmodule Exownet.OWClient do
     }
   end
 
-  @spec ping(t(), list(atom)) :: {:ok, t()} | error_tuple
+  @doc """
+  Sends a 'No Operation' (NOP) packet to the OWServer, acting as a 'ping'.
+  Can be used to keeps a persistent connection alive.
+
+  ## Params
+  - `client`: The `OWClient` struct
+  - `flags`: Optional. A list of flags to send for this message only.
+
+  ## Returns
+  Returns an `{:ok, t()}` tuple on success, where `t()` is the updated `OWClient` struct. If the server
+  keeps the connection open, it will set the persistence flag in the client struct.
+
+  In case of failure, it returns an error tuple `{:error, reason}`
+
+  """
+  @spec ping(t(), OWPacket.flag_list()) :: {:ok, t()} | error_tuple
   def ping(client, flags \\ []) do
     flags = OWPacket.update_flag(flags, client.flag)
     req_packet = OWPacket.create_packet(:NOP, <<>>, flags)
@@ -36,7 +91,21 @@ defmodule Exownet.OWClient do
     end
   end
 
-  @spec dir(t(), String.t(), list(atom)) :: {:ok, t(), list(String.t())} | error_tuple
+  @doc """
+  Sends a DIRALLSLASH packet to the OWServer. Returns a list of directories and/or endpoints located at the specified path.
+  Directories end with a `/`.
+
+  ## Params
+  - `client`: The `OWClient` struct
+  - `path`: A string representing a directory to list the contents of. Defaults to "/"
+  - `flags`: Optional. A list of flags to send for this message only.
+
+  ## Returns
+  Returns a tuple of `{:ok, t(), list(String.t())}` where the list of strings are folders and/or endpoints at the path.
+
+  On failure, returns `{:error, reason}`.
+  """
+  @spec dir(t(), String.t(), OWPacket.flag_list()) :: {:ok, t(), list(String.t())} | error_tuple
   def dir(client, path \\ "/", opt_flags \\ []) do
     flags = OWPacket.update_flag(opt_flags, client.flag)
     req_packet = OWPacket.create_packet(:DIRALLSLASH, path <> <<0>>, flags)
@@ -54,7 +123,18 @@ defmodule Exownet.OWClient do
     end
   end
 
-  @spec read(t(), String.t(), list(atom)) :: {:ok, t(), String.t()} | error_tuple
+  @doc """
+  Reads data from the specified path on the OWServer.
+
+  ## Params
+  - `client`: The `OWClient` struct
+  - `path`: A string representing the path you want to read the contents of.
+  - `flags`: Optional. A list of flags to send for this message only.
+
+  ## Returns
+  Returns an tuple of `{:ok, t(), <<>>}` where the bitstring contains the read value
+  """
+  @spec read(t(), String.t(), OWPacket.flag_list()) :: {:ok, t(), <<>>} | error_tuple
   def read(client, path, opt_flags \\ []) do
     flags = OWPacket.update_flag(opt_flags, client.flag)
     req_packet = OWPacket.create_packet(:READ, path <> <<0>>, flags, @maxsize, 0)
@@ -66,7 +146,27 @@ defmodule Exownet.OWClient do
     end
   end
 
-  @spec write(t(), String.t(), binary() | String.t() | boolean | :on | :off, list(atom)) :: {:ok, t(), binary()} | error_tuple
+@doc """
+Writes data to the specified path.
+
+## Parameters
+
+- `client`: The `OWClient` struct which contains connection information.
+- `path`: The path on the server where the value will be written.
+- `value`: The value to write. It can be a binary, string, boolean, :on, or :off. For booleans, :on, and :off,
+  it will be converted to <<"1">> or <<"0">> respectively.
+- `opt_flags`: Optional. A list of atoms representing flags for the client. These flags are updated in the client's
+  current flag state before the write request.
+
+## Returns
+
+Returns an `{:ok, t()}` tuple on success, where `t()` is the updated `OWClient` struct.
+
+In case of failure, it returns an error tuple `{:error, reason}` where `reason` is a string, an integer or an atom
+indicating the reason for the failure.
+
+"""
+  @spec write(t(), String.t(), binary() | String.t() | boolean | :on | :off, OWPacket.flag_list()) :: {:ok, t()} | error_tuple
   def write(client, path, value, opt_flags \\ [])
   def write(client, path, true, opt_flags), do: write(client, path, <<?1>>, opt_flags)
   def write(client, path, :on, opt_flags), do: write(client, path, <<?1>>, opt_flags)
@@ -102,7 +202,7 @@ defmodule Exownet.OWClient do
     {:ok, client}
   end
 
-  @spec update_persistence(t(), binary()) :: t()
+  @spec update_persistence(t(), OWPacket.header()) :: t()
   defp update_persistence(client, header) do
     if OWPacket.persistence_granted?(header) do
       # Keep socket around for next iteration
@@ -126,9 +226,7 @@ defmodule Exownet.OWClient do
   @spec close_socket(:gen_tcp.socket()) :: :ok
   defp close_socket(socket), do: :gen_tcp.close(socket)
 
-
-
-  @spec receive_message(:gen_tcp.socket()) :: {:ok, binary(), binary()} | error_tuple
+  @spec receive_message(:gen_tcp.socket()) :: {:ok, OWPacket.header(), <<>>} | error_tuple
   defp receive_message(socket) do
     with {:ok, header} <- receive_packet(socket, 24) do
       ret_code = OWPacket.return_code(header)
@@ -141,7 +239,7 @@ defmodule Exownet.OWClient do
     end
   end
 
-  @spec receive_payload(:gen_tcp.socket(), binary()) :: {:ok, binary(), binary()} | error_tuple
+  @spec receive_payload(:gen_tcp.socket(), OWPacket.header()) :: {:ok, OWPacket.header(), <<>>} | error_tuple
   defp receive_payload(socket, header) do
     payload_size = OWPacket.payload_size(header)
 
@@ -156,7 +254,7 @@ defmodule Exownet.OWClient do
     end
   end
 
-  @spec receive_non_empty_message(:gen_tcp.socket()) :: {:ok, binary(), binary()} | error_tuple
+  @spec receive_non_empty_message(:gen_tcp.socket()) :: {:ok, OWPacket.header(), <<>>} | error_tuple
   defp receive_non_empty_message(socket) do
     # Keeps calling receive_message until it receives a message with a payload or an error is returned
     case receive_message(socket) do
--- lib/exownet/owpacket.ex
+++ lib/exownet/owpacket.ex
@@ -2,7 +2,58 @@
 defmodule Exownet.OWPacket do
   import Bitwise
 
-  @message_type %{
+
+  @moduledoc false
+
+  # The `Exownet.OWPacket` module provides functionality for creating and manipulating owserver (One-Wire File System) packets.
+  #
+  # This module allows you to create packets with `create_packet/5`, update packet flags with `update_flag/2`, and check for granted persistence with `persistence_granted?/1`.
+  #
+  # The module also provides several decoding utilities:
+  # - `decode_outgoing_packet_header/1`
+  # - `decode_incoming_packet_header/1`
+  #
+  # The module defines the following types:
+  # - `message_type`: Represents different types of owserver messages.
+  # - `flag_value`: Represents different flags that can be set on an OWFS message.
+  # - `flag_list`: Represents a list of `flag_value`s.
+  # - `header`: Represents the binary header of an owserver packet.
+  # - `packet`: Represents an entire OWFS packet, including the header and payload.
+  # - `decoded_outgoing_packet_header`: Represents the structure of a decoded outgoing packet header as a map.
+  # - `decoded_incoming_packet_header`: Represents the structure of a decoded incoming packet header as a map.
+  #
+  # The module also defines constants for message types and flags. These constants include various options and scales (for temperature, pressure, and address displays) that are used in OWFS messages.
+
+  @type message_type :: :ERROR | :NOP | :READ | :WRITE | :DIR | :SIZE | :PRESENT | :DIRALL | :GET | :DIRALLSLASH | :GETSLASH
+  @type flag_value :: :uncached | :safemode | :alias | :persistence | :bus_ret | :ownet | :c | :f | :k | :r |
+                      :mbar | :atm | :mmhg | :inhg | :psi | :pa | :fdi | :fi | :fdidc | :fdic | :fidc | :fic
+  @type flag_list :: list(flag_value())
+  @type header :: <<_:: 192>> #header consists of 6 * 32-bit integers
+  @type packet :: <<_:: 192, _::_*1>> #header + payload
+
+  @type decoded_outgoing_packet_header :: %{
+    :version => integer(),
+    :payloadsize => integer(),
+    :type => integer(),
+    :flag => integer(),
+    :size => integer(),
+    :offset => integer()
+  }
+
+  @type decoded_incoming_packet_header :: %{
+    :flag => integer(),
+    :offset => integer(),
+    :payloadsize => integer(),
+    :ret => integer(),
+    :size => integer(),
+    :version => integer()
+  }
+
+  @version 0
+  @expected_size 0
+  @offset 0
+
+  @message_types %{
     ERROR: 0,
     NOP: 1,
     READ: 2,
@@ -15,10 +66,6 @@ defmodule Exownet.OWPacket do
     DIRALLSLASH: 9,
     GETSLASH: 10
   }
-
-  @version 0
-  @expected_size 0
-  @offset 0
 
   @flags %{
     # owfs options
@@ -52,6 +99,24 @@ defmodule Exownet.OWPacket do
     fic: 0x05000000    #fic (/42C2D15400000009/)
   }
 
+
+
+  @doc """
+  Creates an outgoing OWFS packet with the given parameters.
+
+  ## Params
+
+  - `type`: A `message_type` that represents the type of the message. This should be one of the values defined in `@message_types`.
+  - `payload`: A `bitstring` that represents the payload of the packet.
+  - `flags`: An `integer` that represents the flags to be set for the packet. Flags should be derived from `@flags`.
+  - `expected_size` (optional): An `integer` that represents the expected size of the packet. Defaults to `@expected_size`.
+  - `offset` (optional): An `integer` that represents the offset of the packet. Defaults to `@offset`.
+
+  ## Returns
+
+  - A `packet` which is a binary that represents the OWFS packet. This binary includes the packet header and the payload.
+  """
+  @spec create_packet(message_type(), bitstring(), integer(), integer(), integer()) :: packet()
   def create_packet(
         type,
         payload,
@@ -59,39 +124,53 @@ defmodule Exownet.OWPacket do
         expected_size \\ @expected_size,
         offset \\ @offset
       ) do
-    header = <<@version::32-big, byte_size(payload)::32-big, @message_type[type]::32-big,
+    header = <<@version::32-big, byte_size(payload)::32-big, @message_types[type]::32-big,
               flags::32-big, expected_size::32-big, offset::32-big>>
 
     header <> payload
   end
 
-  def update_flag(flags, current_value) do
-      Enum.reduce(flags, current_value, fn flag, acc -> @flags[flag] ||| acc end)
+  @spec update_flag(flag_list(), integer()) :: integer()
+  def update_flag(flags, current_value \\ 0) do
+    # Takes a list of flags and the current flag value, and computes a new flag value.
+    # This will allow you to set invalid flag combinations (e.g. setting multiple temperature scales)
+    Enum.reduce(flags, current_value, fn flag, acc -> @flags[flag] ||| acc end)
   end
 
-  # https://owfs.org/index_php_page_tcp-messages.html
 
+  @spec persistence_granted?(header()) :: boolean()
   def persistence_granted?(header) do
+    # Returns true if the provided header's flag value has set the "persistence" value to true. This indicates the owserver will
+    # keep the socket open to receive another command.
     flag = flags(header)
     (flag &&& @flags[:persistence]) == @flags[:persistence]
   end
 
+  @spec payload_size(header()) :: integer()
   def payload_size(header) do
+    # Returns the size of the payload that follows the header.
     <<_version::32, payloadsize::32-integer-signed-big, _rest::binary>> = header
     payloadsize
   end
 
+  @spec return_code(header()) :: integer()
   def return_code(header) do
+    # Returns the return code. The return code is a header value that indicates either success (0) or an error code.
+    # Error codes are always negative.
     <<_version_payload::64, ret_code::32-integer-signed-big, _rest::binary>> = header
     ret_code
   end
 
+  @spec flags(header()) :: integer()
   def flags(header) do
+    # Returns the flag value of the supplied header.
     <<_version_payload_type::96, header::32-integer-signed-big, _rest::binary>> = header
     header
   end
 
+  @spec decode_outgoing_packet_header(header()) :: decoded_outgoing_packet_header()
   def decode_outgoing_packet_header(data) do
+    # Returns a map representation of an outgoing header.
     <<version::32-integer-signed-big, payloadsize::32-integer-signed-big,
       type::32-integer-signed-big, flag::32-integer-signed-big, size::32-integer-signed-big,
       offset::32-integer-signed-big>> = data
@@ -106,7 +185,9 @@ defmodule Exownet.OWPacket do
     }
   end
 
+  @spec decode_incoming_packet_header(header) :: decoded_incoming_packet_header()
   def decode_incoming_packet_header(data) do
+    # Returns a map representation of an incoming header.
     <<version::32-integer-signed-big, payloadsize::32-integer-signed-big,
       ret::32-integer-signed-big, flag::32-integer-signed-big, size::32-integer-signed-big,
       offset::32-integer-signed-big>> = data
